b65b72b7e13a6f318ce7cb73b747fd4314464d20
liz rush <liz@lizmrush.com>
2013-11-11 16:59:57 -0800

    Edits market class methods & Vendor class methods

--- lib/market.rb
+++ lib/market.rb
@@ -4,8 +4,13 @@ class Market
   attr_accessor :name, :address, :city, :state, :county, :zip, :id
 
   def initialize(array)
-  #  @name = "blah"
-  #  @address = "whatever"
+    @name = array[1]
+    @id = array[0]
+    @address = array[2]
+    @city = array[3]
+    @county = array[4]
+    @state = array[5]
+    @zip = array[6]
   end
 
   def self.file_location
@@ -19,47 +24,53 @@ class Market
   end
 
   def self.find(id)
+    market = Market.new(CSV.read(file_location).find do |array|
+      array[0].to_i == id.to_i
+    end)   
+  end
+# this code does not work. must rework
+  # def self.find_all_by_city(match)
+  #   array2 = []
+  #   CSV.read(file_location).select do |array|
+  #      if array[3].downcase == match.downcase
+  #       market = Market.new(array)
+  #       array2 << market
+  #     end
+  #   end
+  #   puts array2
+  # end
+
+  def self.find_all_by_state(match)
     CSV.read(file_location).select do |array|
-      array[0] == id
+      array[5].downcase == match.downcase
     end
   end
 
-  def self.find_by_city(match)
+  def self.find_all_by_county(match)
     CSV.read(file_location).select do |array|
-      array[3] == match
+      array[4].downcase == match.downcase
     end
   end
 
-  def self.find_by_state(match)
+  def self.find_all_by_zip(match)
     CSV.read(file_location).select do |array|
-      array[5] == match
+      array[-1].to_i == match.to_i
     end
   end
 
-  def self.find_by_county(match)
+  def self.find_all_by_address(match)
     CSV.read(file_location).select do |array|
-      array[4] == match
+      array[2].downcase == match.downcase
     end
   end
 
-  def self.find_by_zip(match)
+  def self.find_all_by_name(match)
     CSV.read(file_location).select do |array|
-      array[-1] == match
+      array[1].downcase == match.downcase
     end
   end
 
-  def self.find_by_address(match)
-    CSV.read(file_location).select do |array|
-      array[2] == match
-    end
-  end
-
-  def self.find_by_name(match)
-    CSV.read(file_location).select do |array|
-      array[1] == match
-    end
-  end
-
+  # def 
   # private
   # def read_file
   #   CSV.read(file_location)
--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,4 +1,40 @@
 class Vendor
 
+  attr_accessor :id, :name, :num_of_employees, :market_id
+
+  def self.file_location
+    "./support/vendors.csv"
+  end
+
+  def self.all
+    CSV.read(file_location).map do |array|
+      Market.new(array)
+    end
+  end
+
+
+  def self.find(id)
+    CSV.read(file_location).find do |array|
+      array[0].to_i == id.to_i
+    end
+  end
+
+    def self.find_all_by_name(match)
+    CSV.read(file_location).select do |array|
+      array[1].downcase == match.downcase
+    end
+  end
+
+    def self.find_all_by_num_of_employee(match)
+    CSV.read(file_location).select do |array|
+      array[2].downcase == match.downcase
+    end
+  end
+
+    def self.find_all_by_market_id(match)
+    CSV.read(file_location).select do |array|
+      array[3].to_i == match.to_i
+    end
+  end
 
 end
